86cd09de18917f7af88e5ec34f9c996fe9ef2dc6
youaintmine <ahmbarishsaikia103@gmail.com>
2023-07-26 00:38:49 +0530

    Init File Philosophers Problem

--- src/com/youaintmine/multithreading/philosophersexample/App.java
+++ src/com/youaintmine/multithreading/philosophersexample/App.java
@@ -2,6 +2,6 @@ package com.youaintmine.multithreading.philosophersexample;
 
 public class App {
     public static void main(String[] args) {
-
+        System.out.println("Philosophers Problem");
     }
 }
